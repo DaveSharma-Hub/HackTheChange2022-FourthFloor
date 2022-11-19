348f86d20638384510c01691b593fd76f218acd1
DaveSharma-Hub <davesharma12345@outlook.com>
2022-11-19 11:46:16 -0700

    listings page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
+import Listings from './Pages/Listings';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-        {/* <Route path="/" element={<Home />}/> */}
+        <Route path="/listings" element={<Listings />}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Pages/Listings.js
@@ -0,0 +1,14 @@
+import Header from "../Components/Header";
+
+function Listings(){
+    return(
+        <div>
+            <Header />
+            <div>
+                Listings
+            </div>
+        </div>
+    )
+}
+
+export default Listings;
