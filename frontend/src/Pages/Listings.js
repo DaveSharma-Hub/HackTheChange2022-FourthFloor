834a6539c7c2f9e400abe48594a86f41b366c0b1
DaveSharma-Hub <davesharma12345@outlook.com>
2022-11-19 15:14:54 -0700

    popup-card styling

--- frontend/src/Pages/Listings.js
+++ frontend/src/Pages/Listings.js
@@ -12,13 +12,22 @@ function Listings(){
             description:"Cool supplies and stuff"
         }
     ];
+
+    const data = {
+        img:"https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Calgary_Dinos_logo.svg/800px-Calgary_Dinos_logo.svg.png",
+        itemList:["Books","Pencils"],
+        total:"123",
+        progress:"54",
+        description:"Cool supplies and stuff and more stuff"
+    };
+
     return(
         <div className="listings">
             <Header />
             <div className="listingCards">
                 {
                     [...Array(26)].map((item,index)=>{
-                        return <Card key={index}/>
+                        return <Card key={index} data={data}/>
                     })
                 
                 }
