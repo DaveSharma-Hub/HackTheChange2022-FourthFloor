bb73d353e31aa073e5b671700b60530e0a7081ac
ManjotSin <81481599+ManjotSin@users.noreply.github.com>
2022-11-19 15:13:56 -0700

    added homepage changes

--- frontend/src/Components/MissionStatement.js
+++ frontend/src/Components/MissionStatement.js
@@ -5,15 +5,21 @@ function MissionStatement(){
         <div>
             <div className="missionstatement">
                 <div className="missionstatement_image">
-                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/98/Calgary_Dinos_logo.svg/800px-Calgary_Dinos_logo.svg.png" alt="image"/>
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/f/fb/UofCCoat.svg/1200px-UofCCoat.svg.png" alt="image"/>
                 </div>
                 <div className="statement_button">
                     <h2>Help Students Now!</h2>
                     <button>Donate</button>
                 </div>
-                
             </div>
-            <h1>Mission Statement</h1>
+            <div className ="mission">
+                <h1 className = "h1">The Mission</h1>
+                <p className = "p">Supplied aims to provide all U of C students the tools they need to succeed at University. Students in need will be able to get any supplies they require thanks to our helpful supporters. Our goal is for all students to be able to learn!</p>
+            </div>
+            <div className = "about">
+                <h1 className>About Us</h1>
+                <p className = "p">Supplied started during the YYC Code The Change 2022 Hackathon, where our team of 5 had an idea that could help all students reach their educatonal goals.</p>
+            </div>
         </div>
     )
 }
