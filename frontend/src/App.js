cf1132dabca50bbc9b60885e0250809c777ec567
DaveSharma-Hub <davesharma12345@outlook.com>
2022-11-19 21:14:18 -0700

    donate money works!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home';
 import Listings from './Pages/Listings';
 import DonateForm from './Pages/DonateForm';
+import listingData from './database';
+import {useState} from 'react';
 
 function App() {
+  const [data,setData] = useState(listingData);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
-        <Route path="/listings" element={<Listings />}/>
-        <Route path="/donate" element={<DonateForm />}/>
+        <Route path="/listings" element={<Listings listingData={data}/>}/>
+        <Route path="/donate" element={<DonateForm data={data} setData={setData}/>}/>
         {/* <Route path="/used" element={<Used />} /> */}
       </Routes>
     </BrowserRouter>
--- frontend/src/Pages/DonateForm.js
+++ frontend/src/Pages/DonateForm.js
@@ -1,20 +1,49 @@
 import Header from "../Components/Header";
 import './donateform.scss'
 import {useState} from 'react';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 
-function DonateForm(){
+function DonateForm({data,setData}){
     const [amount, setAmount] = useState(5);
     const [name,setName] = useState('');
     const [clickedFive,setClickedFive] = useState(true);
     const [clickedTwenty,setClickedTwenty] = useState(false);
     const [clickedFourty,setClickedFourty] = useState(false);
-
+    const [searchParams, setSearchParams] = useSearchParams();
+    let id = searchParams.get('id');
+    const history = useNavigate();
 
     const handleSetClicked = (setState,setOther1,setOther2) =>{
         setState(true);
         setOther1(false);
         setOther2(false);
     }
+
+    const getIndex = (data) =>{
+       for(let i=0;i<data.length;i=i+1){
+        console.log(data[i].id)
+        if(data[i].id===parseInt(id)){
+            return i;
+        }
+       }
+       return null;
+    }
+
+    const handleSubmitDonation = (e) =>{
+        e.preventDefault();
+        const tmp = data;
+        const index = getIndex(tmp);
+        console.log(index);
+        if(index!=null){
+            tmp[index].progress = parseInt(tmp[index].progress) + amount;
+            setData(tmp);
+            history('/');
+        }else{
+            alert('Try Again Please!');
+        }
+    }
+
+
 
     return(
         <div>
@@ -24,9 +53,9 @@ function DonateForm(){
                     <img src="https://media.istockphoto.com/id/1370856587/photo/cropped-portrait-of-an-attractive-young-female-student-using-her-laptop-to-study-outside-on.jpg?b=1&s=170667a&w=0&k=20&c=iugztx7miDp42Pi5UKHaXq5VisV8GG0g1kBJhz1r6R8=" alt="image"/>
                 </div>
                 <div className = "form">
-                    <form>
+                    <div >
                         <h1>Make an online donation.</h1>
-                        <form onSubmit={()=>{}}>
+                        <form onSubmit={(e)=>{handleSubmitDonation(e)}}>
                             <input placeholder="Name(optional)" onChange={(e)=>{setName(e.target.value)}}/>
                             <button className={clickedFive ? "clicked" : "notClicked"} 
                                 onClick={()=>{
@@ -49,7 +78,7 @@ function DonateForm(){
                                 }}/>
                             <button type="submit">Donate</button>
                         </form>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
