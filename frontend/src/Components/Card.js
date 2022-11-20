106befbae5f4eef9f201e0750f2314590070cde1
DaveSharma-Hub <davesharma12345@outlook.com>
2022-11-19 19:22:00 -0700

    changes

--- frontend/src/Components/Card.js
+++ frontend/src/Components/Card.js
@@ -8,18 +8,28 @@ function Card({data}){
     
     const percent = (parseFloat(data.progress)/parseFloat(data.total))*100;
     const percentString = percent.toString().split('.')[0]
-    console.log(percent);
+
     return(
         <div className='card'>
             <div className='itemimg'>
                 <img src = {data.img}></img>
             </div>
             <div className='description'>
-                <h1>
+                <h1 className="descriptionCard">
                     {data.description}
                 </h1>
+                <h1 className='raised'>
+                    ${data.progress} raised
+                </h1>
+                    {/* <h1>
+                    • 
+                    </h1> */}
+                <hr />
+                <h1 className='goal'>
+                    ${data.total} goal
+                </h1>
             </div>
-            <div className="progress">
+            {/* <div className="progress">
                 <div className='progressbar'>
                     <div class="wrap-circles">
                         <div class="circle" style={{backgroundImage: `conic-gradient(#B5838D ${percentString}%, #FFCDB2 0)`}}>
@@ -31,17 +41,18 @@ function Card({data}){
                     <h1>
                         ${data.progress} raised
                     </h1>
-                    {/* <h1>
-                    • 
-                    </h1> */}
+                    
                     <hr />
                     <h1>
                     ${data.total} goal
                     </h1>
                 </div>
-            </div>
-            <div className="donateDiv">
+            </div> */}
+            {/* <div className="donateDiv">
                 <button className='donateButtonCard' onClick={()=>{history(`/donate?id=${data.id}`)}}>Donate Here</button>
+            </div> */}
+            <div className="donateDiv">
+                <button className='donateButtonCard' onClick={()=>{}}>View</button>
             </div>
         </div>
     )
